cacd813b1d7480e20da368df0396050b805ebe9c
thilinajayanath <thn@alteos.com>
2024-11-20 09:18:39 +0100

    feat: go for clis

--- pages/presentations/_meta.ts
+++ pages/presentations/_meta.ts
@@ -3,4 +3,5 @@ export default {
   "241024-vercel-nextra": "24.10.24 - Vercel + Nextra",
   "241031-ollama": "31.10.24 - Ollama Local LLMs",
   "241107-slidev": "07.11.24 - Slidev",
+  "241114-go-for-cli": "14.11.24 - Go for CLI tools",
 };
