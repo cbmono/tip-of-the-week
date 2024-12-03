08b941d90c9f53f976e3d8e8d6275a3334692815
majidyaghouti <may@alteos.com>
2024-12-03 16:21:00 +0100

    add pomodoro presentation

--- pages/presentations/_meta.ts
+++ pages/presentations/_meta.ts
@@ -3,4 +3,5 @@ export default {
   "241024-vercel-nextra": "24.10.24 - Vercel + Nextra",
   "241031-ollama": "31.10.24 - Ollama Local LLMs",
   "241128-communication": "28.11.24 - Communication",
+  "241205-pomodoro": "05.12.24 - Pomodoro",
 };
